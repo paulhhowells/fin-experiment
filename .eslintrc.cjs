2c969fa94bd6ef9bc40f70bfcdd209091e398ef2
Paul H Howells <p.h.howells@gmail.com>
2024-05-22 12:30:25 +0100

    chore: add lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'indent': [ 'error', 2, { 'SwitchCase': 1 } ],
+    'quotes': [ 'error', 'single', { 'allowTemplateLiterals': true } ],
   },
 }
