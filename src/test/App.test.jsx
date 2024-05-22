3c359aa24b52267d9878589b2ae3c0445f731f61
Agata Jewko <agata@jewko.co.uk>
2024-05-22 13:01:55 +0100

    TODO tests

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -2,8 +2,16 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App Component', () => {
-  it('renders the correct content', () => {
+  it('renders the App', () => {
     render(<App />);
     expect(screen.getByText('Vite + React')).toBeInTheDocument();
   });
+
+  it.todo('renders the data from the API', () => {
+
+  });
+
+  it.todo('renders an error if the API call fails', () => {
+  });
+  
 });
